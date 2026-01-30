270217404de8ab4f9f9ef83469fd043820fdaff8
Ivan_Moroz <ivan_moroz@epam.com>
2026-01-30 16:31:35 +0200

    added app to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 
 export default function HomePage() {
     return (
-        <>
+        <div className='app'>
             <h1>React TypeScript application</h1>
             <p>This app includes lazy loading of JavaScript, a router, and several simple components written in React + TypeScript. Some parts are generated using AI.</p>
-        </>
+        </div>
 
     );
 }
