85f9f5d14894ed9b7fdd84d46fc002da7d201836
Ivan_Moroz <ivan_moroz@epam.com>
2026-02-03 18:10:38 +0200

    Added first success test

--- src/components/toDo/ToDo.test.tsx
+++ src/components/toDo/ToDo.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ToDo from './ToDo';
 
-test('renders todo List', () => {
-  render(<App />);
+test('Renders ToDo List', () => {
+  render(<ToDo />);
   const linkElement = screen.getByText(/ToDo List/i);
   expect(linkElement).toBeInTheDocument();
 });
