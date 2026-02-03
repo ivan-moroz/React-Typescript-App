184320915145d4c9c0cc8a5e5c1f87528b8b5c2e
Ivan_Moroz <ivan_moroz@epam.com>
2026-02-03 18:12:32 +0200

    Refactored first success test

--- src/components/toDo/test/ToDo.test.tsx
+++ src/components/toDo/test/ToDo.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ToDo from './ToDo';
+import ToDo from '../ToDo';
 
 test('Renders ToDo List', () => {
   render(<ToDo />);
