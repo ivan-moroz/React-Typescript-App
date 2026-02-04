f538dd7482389ab2069c054dc92b1ed5c14fe162
Ivan_Moroz <ivan_moroz@epam.com>
2026-02-04 14:58:20 +0200

    Changed home description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
     return (
         <div className='app'>
             <h1>React TypeScript application</h1>
-            <p>This app includes lazy loading of JavaScript, a router, and several simple components written in React + TypeScript. Some parts are generated using AI.</p>
+            <p>This app features lazy loading of JavaScript, a router, and several simple components written in React and TypeScript, covered by Jest tests. Some parts are generated using AI.</p>
         </div>
 
     );
